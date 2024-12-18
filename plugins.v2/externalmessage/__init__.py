9196ca563126009d52e5535a730fad03899669e6
KoWming <1075151242@qq.com>
2024-12-18 15:45:18 +0800

    up

--- plugins.v2/externalmessage/__init__.py
+++ plugins.v2/externalmessage/__init__.py
@@ -23,7 +23,7 @@ class ExternalMessage(_PluginBase):
     # 插件图标
     plugin_icon = "forward.png"
     # 插件版本
-    plugin_version = "0.2"
+    plugin_version = "0.3"
     # 插件作者
     plugin_author = "KoWming"
     # 作者主页
