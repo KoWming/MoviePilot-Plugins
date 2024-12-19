5001f64830776faa559b309bd030f4ae444b3324
KoWming <1075151242@qq.com>
2024-12-19 10:15:54 +0800

    test

--- plugins/mgynotify/__init__.py
+++ plugins/mgynotify/__init__.py
@@ -1,35 +1,25 @@
-import requests, json
-from typing import Any, List, Dict, Tuple, Optional
-from pathlib import Path
-
-from app.log import logger
-from app.chain.dashboard import DashboardChain
-from app.core.config import settings
-from app.db.subscribe_oper import SubscribeOper
-from app.helper.directory import DirectoryHelper
 from app.plugins import _PluginBase
-from app.core.event import eventmanager, Event
-from app.schemas.types import EventType, NotificationType
+from typing import Any, List, Dict, Tuple
+from app.log import logger
+from app.schemas import NotificationType
 from app import schemas
-from app.utils.system import SystemUtils
-from app.utils.http import RequestUtils
 
 
-class ExternalMessage(_PluginBase):
+class MgNotify(_PluginBase):
     # 插件名称
-    plugin_name = "External Message"
+    plugin_name = "魔改Webhook通知"
     # 插件描述
-    plugin_desc = "外部应用消息推送。"
+    plugin_desc = "接收webhook通知并推送。"
     # 插件图标
-    plugin_icon = "forward.png"
+    plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/synology.png"
     # 插件版本
-    plugin_version = "0.3"
+    plugin_version = "1.3"
     # 插件作者
-    plugin_author = "KoWming"
+    plugin_author = "thsrite"
     # 作者主页
-    author_url = "https://github.com/KoWming/MoviePilot-Plugins"
+    author_url = "https://github.com/thsrite"
     # 插件配置项ID前缀
-    plugin_config_prefix = "externalmessage_"
+    plugin_config_prefix = "mgnotify_"
     # 加载顺序
     plugin_order = 30
     # 可使用的用户级别
@@ -37,46 +27,36 @@ class ExternalMessage(_PluginBase):
 
     # 任务执行间隔
     _enabled = False
+    _notify = False
+    _msgtype = None
 
     def init_plugin(self, config: dict = None):
         if config:
             self._enabled = config.get("enabled")
+            self._notify = config.get("notify")
+            self._msgtype = config.get("msgtype")
+
+    def mg_notify(self, title: str, text: str) -> schemas.Response:
+        """
+        发送通知
+        """
+        logger.info(f"收到webhook消息啦,{title}\n{text}")
+        if self._enabled and self._notify:
+            mtype = NotificationType.Manual
+            if self._msgtype:
+                mtype = NotificationType.__getitem__(str(self._msgtype)) or NotificationType.Manual
+            self.post_message(title=title,
+                              mtype=mtype,
+                              text=text)
+
+        return schemas.Response(
+            success=True,
+            message="发送成功"
+        )
 
     def get_state(self) -> bool:
         return self._enabled
 
-    def send_json(self, apikey: str, data="") -> schemas.Response:
-        """
-        解析接收到的POST请求中的JSON数据，然后调用post_message方法发送消息。
-        """
-        if apikey != settings.API_TOKEN:
-            return schemas.Response(success=False, message="API密钥错误")
-            
-        # 将接收到的数据转换为字符串,并解析JSON数据
-        # data_str = data.decode('utf-8')
-        json_data = json.loads(data)
-        if not json_data:
-            logger.warn("请求体为空或格式不正确")
-            return schemas.Response(success=False, message="请求体为空或格式不正确")
-            
-        # 提取title和text字段
-        title = json_data.get('title')
-        content = json_data.get('content')
-        if not title or not content:
-            logger.warn("缺少必要的字段title或content")
-            return schemas.Response(success=False, message="缺少必要的字段title或content")
-            
-        # 记录title和text到日志
-        logger.info(f"Received title: {title}, text: {content}")
-
-        # 调用post_message方法发送消息
-        self.post_message(
-            mtype=NotificationType.Plugin,
-            title=title,
-            content=content
-            )
-        return schemas.Response(success=True, message="消息接收成功", data={"title": title, "content": content})
-        
     @staticmethod
     def get_command() -> List[Dict[str, Any]]:
         pass
@@ -92,11 +72,11 @@ class ExternalMessage(_PluginBase):
         }]
         """
         return [{
-            "path": "/send_json",
-            "endpoint": self.send_json,
+            "path": "/mgwebhook",
+            "endpoint": self.mg_notify,
             "methods": ["POST"],
-            "summary": "外部应用自定义消息接口使用的API",
-            "description": "接收外部应用的json自定义消息接口",
+            "summary": "魔改webhook",
+            "description": "接受webhook通知并推送",
         }]
 
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
@@ -133,6 +113,22 @@ class ExternalMessage(_PluginBase):
                                     }
                                 ]
                             },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 6
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'notify',
+                                            'label': '开启通知',
+                                        }
+                                    }
+                                ]
+                            },
                         ]
                     },
                     {
@@ -141,21 +137,18 @@ class ExternalMessage(_PluginBase):
                             {
                                 'component': 'VCol',
                                 'props': {
-                                    'cols': 12,
+                                    'cols': 12
                                 },
                                 'content': [
                                     {
-                                        'component': 'VAlert',
+                                        'component': 'VSelect',
                                         'props': {
-                                            'type': 'success',
-                                            'variant': 'tonal'
-                                        },
-                                        'content': [
-                                            {
-                                                'component': 'span',
-                                                'text': 'API接口地址参考：http://MoviePilot_IP:PORT/api/v1/plugin/ExternalMessage/send_json?apikey=api_token'
-                                            },
-                                        ]
+                                            'multiple': False,
+                                            'chips': True,
+                                            'model': 'msgtype',
+                                            'label': '消息类型',
+                                            'items': MsgTypeOptions
+                                        }
                                     }
                                 ]
                             }
@@ -175,7 +168,29 @@ class ExternalMessage(_PluginBase):
                                         'props': {
                                             'type': 'info',
                                             'variant': 'tonal',
-                                            'text': '如安装完启用插件后回调地址无响应，重启MoviePilot即可。'
+                                            'text': 'webhook配置http://ip:3001/api/v1/plugin/MgNotify/mgwebhook?apikey=*****&text=hello world。'
+                                                    'text参数类型是消息内容。此插件安装完需要重启生效api。消息类型默认为手动处理通知。'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'tonal',
+                                            'text': '如安装完插件后，发送webhook提示404，重启MoviePilot即可。'
                                         }
                                     }
                                 ]
@@ -186,14 +201,13 @@ class ExternalMessage(_PluginBase):
             }
         ], {
             "enabled": False,
+            "notify": False,
+            "msgtype": ""
         }
 
     def get_page(self) -> List[dict]:
-        """
-        拼装插件详情页面，需要返回页面配置，同时附带数据
-        """
         pass
-    
+
     def stop_service(self):
         """
         退出插件
