5c49e23ddd1e343026f5cd74528aeea985b2d066
KoWming <1075151242@qq.com>
2025-06-01 16:57:33 +0800

    Update __init__.py
    修复咪咕视频封面图片加载错误

--- plugins.v2/exploreservices/__init__.py
+++ plugins.v2/exploreservices/__init__.py
@@ -21,7 +21,7 @@ class ExploreServices(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/KoWming/MoviePilot-Plugins/main/icons/ExploreServices.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "KoWming"
     # 作者主页
